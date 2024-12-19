e0d34ccb41b97d70696b2d091a19fe6b0e5d26f6
tayyibaraashid@gmail.com <tayyibaraashid@gmail.com>
2024-12-19 14:57:38 +0500

    minor changes

--- src/app/components/HorizontalScroll.jsx
+++ src/app/components/HorizontalScroll.jsx
@@ -31,7 +31,7 @@ export default function HorizontalScroll() {
         pin: true,
         scrub: 1,
         end: () => container.scrollWidth - window.innerWidth,
-        markers: true,
+         
       },
     });
 
